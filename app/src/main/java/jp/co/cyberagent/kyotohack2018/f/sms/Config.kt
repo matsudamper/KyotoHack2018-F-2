08c3234d0a7cd0b9be14444b6807b3a0c44cf399
matsudamper <let@matsudamper.net>
2018-08-27 20:10:46 +0900

    ログイン処理を実装

--- app/src/main/java/jp/co/cyberagent/kyotohack2018/f/sms/Config.kt
+++ app/src/main/java/jp/co/cyberagent/kyotohack2018/f/sms/Config.kt
@@ -1,5 +1,6 @@
 package jp.co.cyberagent.kyotohack2018.f.sms
 
 object Config {
-    val DEVELOPER_KEY: String? = "b9ab1720f64cd9c8340fbffc423c02581e166418"
+    val DEVELOPER_KEY = "b9ab1720f64cd9c8340fbffc423c02581e166418"
+    val ANDROID_KEY = "354010538089-tfth05ac271ltcikbl8ps2njlc2m72gl.apps.googleusercontent.com"
 }
--- app/src/main/java/jp/co/cyberagent/kyotohack2018/f/sms/di/ApplicationModule.kt
+++ app/src/main/java/jp/co/cyberagent/kyotohack2018/f/sms/di/ApplicationModule.kt
@@ -1,5 +1,6 @@
 package jp.co.cyberagent.kyotohack2018.f.sms.di
 
+import com.google.firebase.auth.FirebaseAuth
 import jp.co.cyberagent.kyotohack2018.f.service.SmsMockService
 import jp.co.cyberagent.kyotohack2018.f.service.SmsService
 import jp.co.cyberagent.kyotohack2018.f.sms.flux.app.AppActionCreator
@@ -19,6 +20,7 @@ import jp.co.cyberagent.kyotohack2018.f.sms.ui.main.flux.MainActivityActionCreat
 import jp.co.cyberagent.kyotohack2018.f.sms.ui.main.flux.MainActivityDispatcher
 import jp.co.cyberagent.kyotohack2018.f.sms.ui.main.flux.MainActivityStore
 import jp.co.cyberagent.kyotohack2018.f.sms.ui.main.home.HomeFragment
+import jp.co.cyberagent.kyotohack2018.f.sms.ui.main.login.LoginFragment
 import jp.co.cyberagent.kyotohack2018.f.sms.ui.main.mypage.MypageFragment
 import jp.co.cyberagent.kyotohack2018.f.sms.ui.main.mypage.flux.MypageActionCreator
 import jp.co.cyberagent.kyotohack2018.f.sms.ui.main.mypage.flux.MypageDispatvher
@@ -61,6 +63,7 @@ val mainActivityModule = module(MainActivity::class.moduleName) {
     factory { HomeFragment() }
     factory { SearchFragment() }
     factory { MypageFragment() }
+    factory { LoginFragment() }
 
     // Flux
     factory { MainActivityActionCreator(get(), get()) }
@@ -68,6 +71,8 @@ val mainActivityModule = module(MainActivity::class.moduleName) {
 
     factory { MypageActionCreator(get()) }
     viewModel { MypageStore(get()) }
+
+    single { FirebaseAuth.getInstance() }
 }
 
 val companyActivityModule = module(CompanyActivity::class.moduleName) {
